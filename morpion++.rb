d60a6ab1e3458521f0b9a5f86c5bcee7c992b97f
koltor <maxence.theme@gmail.com>
2018-01-25 22:40:11 +0100

    OK

--- morpion++.rb
+++ morpion++.rb
@@ -1,23 +1,32 @@
 #cette classe viens regrouper toute les caracteristique d'un joueur
 class Player
 
-	attr_accessor :nom, :forme, :etat
+	attr_accessor :nom, :forme, :etat, :type
 	#lors de la creation on viens demender le nom, la forme ainsi que son tour de jeu
-	def initialize(pseudo,forme,etat)
+	def initialize(pseudo,forme,etat, type)
 		@nom = pseudo
 		@forme = forme
 		@etat = etat
+		@type = type
 	end
 
 	#cette fonction est appeler lors de son tour de jeu
 	#on viens demender la ligne et la colonne que le joueur veux remplir
 	def tourDejeu()
-		puts "A moi de jouer (#{nom})"
-		print "ligne :"
-		x = gets.chomp()
-		print "colonne :"
-		y = gets.chomp()
-		return [x.to_i - 1,y.to_i - 1,@forme]
+		#partie Joueur
+		if @type != "ia"
+			puts "A moi de jouer (#{nom})"
+			print "ligne :"
+			x = gets.chomp()
+			print "colonne :"
+			y = gets.chomp()
+			return [x.to_i - 1,y.to_i - 1,@forme, @type]
+		else
+			sleep(0.5)
+			x = rand(1..3)
+			y = rand(1..3)
+			return [x.to_i - 1,y.to_i - 1,@forme, @type]
+		end
 	end
 end
 
@@ -36,13 +45,16 @@ class Board
 
 	#la fonction d'affichage viens tout simplement afficher le plateau de jeu
 	def aff()
-			print @plateau[0][0],"|", @plateau[0][1],"|",@plateau[0][2]
+				for q  in 1..20
+					puts
+				end
+			print @plateau[0][0]," | ", @plateau[0][1]," | ",@plateau[0][2]
 			puts
-			puts "-----"
-			print @plateau[1][0],"|", @plateau[1][1],"|",@plateau[1][2]
+			puts "---------"
+			print @plateau[1][0]," | ", @plateau[1][1]," | ",@plateau[1][2]
 			puts
-			puts "-----"
-			print @plateau[2][0],"|", @plateau[2][1],"|",@plateau[2][2]
+			puts "---------"
+			print @plateau[2][0]," | ", @plateau[2][1]," | ",@plateau[2][2]
 			puts
 	end
 
@@ -52,11 +64,13 @@ class Board
 			@plateau[status[0]][status[1]] = status[2]
 			aff()
 		else
-			puts"case déja remplis veillez rejouer"
+			if status[3] != "ia"
+				puts"case déja remplis veillez rejouer"
+			end
 			return false
 		end
 	end
-	#les differente 
+	# liste des differentes façon de gagner
 	def verif
 		if ((@plateau[0][0] == @plateau[0][1] && @plateau[0][1] == @plateau[0][2]) && @plateau[0][0] != " ") 	
 			return true
@@ -88,22 +102,35 @@ end
 
 
 
-
 #la classe principale du jeu
 class Game
 
-		def initialize()
-			i = 1
+		def initialize(a = 1)
+			puts "Lancement de la partie"
+			tab = ["jean", "michel", "patrick" , "maxence", "bilel", "chloe", "kevin", "seb", "paul"]
+			i = a
 			j = 0 
+			if i > 1
+				k = rand(0...tab.length)
+				nom = 
+				player1 = Player.new(tab[k],"X","y","ia")
+			end
+			if i == 3
+				kn = 0
+				while  kn == k
+					kn = rand(0...tab.length)
+				end
+				player2 = Player.new(tab[kn],"O","O","ia")
+			end
 				while i <= 2
 					puts "donne moi le nom du perso " + i.to_s
 					nom = gets.chomp()
-					puts "donne moi la forme que tu a choisi (une lettre)"
-					forme = gets.chomp().slice(1...2)
+					puts "donne moi la forme que tu a choisi"
+					forme = gets.chomp()
 					if i == 1
 						puts "veut tu commencer ? O/y"
 						etat = gets.chomp()
-						player1 = Player.new(nom,forme,etat)
+						player1 = Player.new(nom,forme,etat,"perso")
 					else
 						if etat == "O"
 							etat = "y"
@@ -111,7 +138,7 @@ class Game
 						else
 							etat = "O"
 						end
-						player2= Player.new(nom,forme,etat)
+						player2= Player.new(nom,forme,etat,"perso")
 					end	
 					i += 1
 				end
@@ -125,9 +152,9 @@ class Game
 		#fonction d'annonce
 		def annonce(nom1,nom2)
 			puts "nous allons assiter au combat du siècle"
-			sleep(2)
-			puts "un combat de #{nom1} contre #{nom2}"
 			sleep(1)
+			puts "un combat de #{nom1} contre #{nom2}"
+			sleep(2)
 			puts "c'est partie"
 		end
 
@@ -142,17 +169,48 @@ class Game
 				if board.verif == true
 					player[j%2].etat = "v"
 					puts "fin du game"
+					j -= 1
 				end
 				j += 1
 				t += 1
 			end
-			if t == 9
+			if (t == 9 && player[0].etat != "v" && player[1].etat != "v")
 				puts "match nul"
+			
 			else
 				puts "il y a un gagnant " + player[j%2].nom
 			end
 		end
 end
 
-#lancement d'une partie
-game = Game.new()
+class GameMenu
+
+	def initialize()
+		while true
+			affi()
+			a = gets.chomp
+			case 
+				when a == "1" then Game.new()
+				when a == "2" then Game.new(2)
+				when a == "3" then Game.new(3)
+			else 
+				puts "ERROR"
+			end	
+		end
+	end
+
+	def affi()
+		#game menu
+		puts "GAME Menu-----------------"
+		puts "| 1 - partie simple(pVp) |"
+		puts "| 2 - partie simple(pVia)|"
+		puts "| 3 - patie simple(iaVia)|"
+		puts "-----------------GAME Menu"
+	end
+
+	def tournois()
+
+	end
+end
+
+game =  GameMenu.new()
